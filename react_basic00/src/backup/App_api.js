7d901d3b27de124fcbe0ea51af5559bbbdcd8b1f
choibohui <123dfga@naver.com>
2022-08-24 16:12:04 +0900

    20220824

--- react_basic00/src/backup/App_api.js
+++ react_basic00/src/backup/App_api.js
@@ -2,14 +2,6 @@ import './basic.css';
 import { useEffect, useState } from 'react';
 // useEffect 는 한번만 실행하라는거, 리액트에서는 sideEffect 는 스테이트를 변경시키는거 말고??
 
-
-
-
-// 여기 쌤꺼보고 복붙.. 해야함..!!!
-
-
-
-
 const App = () => {
   const [data, setData] = useState([]);
 
@@ -19,12 +11,17 @@ const App = () => {
       .then(result => setData(result))
   }, []);
 
-  console.log(data[0].id)
+  // axios 
+
+  //JSON.parse()
+
+  // console.log(Object(data[0]).id)
+  // async await ????
 
   return (
     <div>
       {
-        data.map(it => <li key={it.id}>{it.id} {it.title}</li>)
+        data.map(it => <li key={it.id}>{it.id} {it.title}</li>).slice(0, 10)
       }
     </div>
   )
